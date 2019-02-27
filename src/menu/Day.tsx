bc4e4f092b503191c3349fb2d76b5ea57fad2c15
saiboten <saiboten@gmail.com>
2019-02-27 19:57:55 +0100

    Moved generate day into its own component

--- src/menu/Day.tsx
+++ src/menu/Day.tsx
@@ -4,10 +4,8 @@ import styled from "styled-components";
 import { firebase } from "../firebase/firebase";
 import { RecipeType, RecipeWithRatingType } from "../types";
 import nbLocale from "date-fns/locale/nb";
-import { StyledActionButton } from "../components/StyledActionButton";
-import { calculate } from "../calculator/calculate";
 import { RecipeDetails } from "../recipes/RecipeDetail";
-import { minBreakPoint } from "../components/Constants";
+import { GenerateDay } from "./GenerateDay";
 
 interface Props {
   date: Date;
@@ -35,105 +33,6 @@ const initialState: RecipeType = {
   weekdays: [],
   lastTimeSelected: new Date(),
   rating: 1
-};
-
-const storeSelectedRecipe = (date: Date, recipe: RecipeType) => {
-  firebase
-    .firestore()
-    .collection("days")
-    .add({
-      date,
-      recipe: recipe.id
-    });
-
-  firebase
-    .firestore()
-    .collection("recipes")
-    .doc(recipe.id)
-    .set({
-      ...recipe,
-      lastTimeSelected: date
-    });
-};
-
-const findRecipe = (date: Date) => {
-  return new Promise(resolve => {
-    firebase
-      .firestore()
-      .collection("recipes")
-      .onSnapshot(snapshot => {
-        const recipes = snapshot.docs.map((doc: any) => ({
-          id: doc.id,
-          ...doc.data(),
-          lastTimeSelected: doc.data().lastTimeSelected.toDate()
-        }));
-
-        const recipesWithRating = recipes.map(
-          (recipe: RecipeWithRatingType) => ({
-            ...recipe,
-            score: calculate(date, recipe, 50)
-          })
-        );
-        const logThis = recipesWithRating.map(({ name, score }: any) => ({
-          ...score,
-          name
-        }));
-
-        console.log(logThis);
-
-        const bestRecipe = recipesWithRating.reduce(
-          (
-            bestRecipe: RecipeWithRatingType,
-            testRecipe: RecipeWithRatingType
-          ) =>
-            bestRecipe.score.totalScore > testRecipe.score.totalScore
-              ? bestRecipe
-              : testRecipe
-        );
-        resolve(bestRecipe);
-      });
-  });
-};
-
-const StyledButtonContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const GenerateDay = ({ date }: { date: Date }) => {
-  const [recipe, setRecipe]: [RecipeType, any] = useState(initialState);
-  const [showConfirm, setShowConfirm]: [boolean, any] = useState(false);
-  const [stored, setStored]: [boolean, any] = useState(false);
-
-  if (stored) {
-    return <RecipeDetails recipe={recipe} />;
-  }
-
-  return (
-    <div>
-      <RecipeDetails recipe={recipe} />
-      <StyledButtonContainer>
-        <StyledActionButton
-          onClick={() => {
-            setShowConfirm(true);
-            findRecipe(date).then((recipe: any) => setRecipe(recipe));
-          }}
-        >
-          Lag dag
-        </StyledActionButton>
-        {showConfirm && (
-          <StyledActionButton
-            onClick={() => {
-              storeSelectedRecipe(date, recipe);
-              setStored(true);
-            }}
-          >
-            Lagre dag
-          </StyledActionButton>
-        )}
-      </StyledButtonContainer>
-    </div>
-  );
 };
 
 export const Day = ({ date }: Props) => {
