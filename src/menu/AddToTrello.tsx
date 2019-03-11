e858ffd8ac8b603c4622c94f1f2dc73e45c248a2
saiboten <saiboten@gmail.com>
2019-03-11 22:43:05 +0100

    Moved trello integration into it's own omp

---
+++ src/menu/AddToTrello.tsx
@@ -0,0 +1,122 @@
+import React, { useContext, useState } from "react";
+import { StyledActionButton } from "../components/StyledActionButton";
+import { UserDataContext } from "../context/UserDataContext";
+import { RecipeContext } from "../context/RecipeContext";
+import { IngredientsContext } from "../context/IngredientsContext";
+import { Ingredient, RecipeType } from "../types";
+import { firebase } from "../firebase/firebase";
+import { StyledLocalLoader } from "../components/StyledLocalLoader";
+
+interface CreateCardResponse {
+  id: string;
+}
+interface CreateChecklistResponse {
+  id: string;
+}
+
+export const AddToTrello = ({ listOfDays }: { listOfDays: Date[] }) => {
+  const [loading, setLoading]: [boolean, any] = useState(false);
+  const [done, setDone]: [boolean, any] = useState(false);
+
+  const userdata = useContext(UserDataContext).userdata;
+  const recipes = useContext(RecipeContext).recipes;
+  const ingredients = useContext(IngredientsContext).ingredients;
+
+  const addIngredients = async (ingredients: string[], checklistId: string) => {
+    for (let i = 0; i < ingredients.length; i++) {
+      await fetch(
+        `https://api.trello.com/1/checklists/${checklistId}/checkItems?key=${
+          userdata.trelloApiKey
+        }&token=${userdata.trelloApiToken}&name=${ingredients[i]}`,
+        {
+          method: "POST"
+        }
+      );
+    }
+    setLoading(false);
+    setDone(true);
+  };
+
+  const addChecklist = (ingredients: string[], id: string) => {
+    fetch(
+      `https://api.trello.com/1/checklists/?key=${
+        userdata.trelloApiKey
+      }&token=${userdata.trelloApiToken}&idCard=${id}`,
+      {
+        method: "POST"
+      }
+    )
+      .then(data => data.json())
+      .then((data: CreateChecklistResponse) => {
+        addIngredients(ingredients, data.id);
+      });
+  };
+
+  const addCard = (ingredients: string[]) => {
+    fetch(
+      `https://api.trello.com/1/cards?name=Handleliste&pos=top&idList=${
+        userdata.trelloList
+      }&key=${userdata.trelloApiKey}&token=${userdata.trelloApiToken}`,
+      {
+        method: "POST"
+      }
+    )
+      .then((data: any) => data.json())
+      .then((data: CreateCardResponse) => {
+        addChecklist(ingredients, data.id);
+      });
+  };
+
+  const addWeekToTrello = (listOfDays: Date[]) => {
+    setLoading(true);
+    const db = firebase.firestore();
+    const promiseList: any = [];
+    listOfDays.forEach((day: any) => {
+      promiseList.push(
+        db
+          .collection("days")
+          .where("date", "==", day)
+          .get()
+      );
+    });
+
+    Promise.all(promiseList).then(data => {
+      const res = data.reduce((init: any, next: any) => {
+        init.push(next.docs[0].data());
+        return init;
+      }, []);
+      const recipeIds = res
+        .filter((el: any) => el.recipe)
+        .map((el: any) => el.recipe);
+      const recipesThisWeek: RecipeType[] = recipeIds.map((recipeId: string) =>
+        recipes.find(el => el.id === recipeId)
+      );
+
+      const ingredientsThisWeek = recipesThisWeek
+        .reduce((init: any, recipe: RecipeType) => {
+          const ingredientsOnThisRecipe = recipe.ingredients.map(ingredientId =>
+            ingredients.find(el => el.id === ingredientId)
+          );
+
+          return init.concat(ingredientsOnThisRecipe);
+        }, [])
+        .map((ingredient: Ingredient) => ingredient.name);
+
+      addCard(ingredientsThisWeek);
+    });
+  };
+
+  if (loading) {
+    return <StyledLocalLoader />;
+  }
+
+  if (done) {
+    return <div>Handleliste generert</div>;
+  }
+
+  return (
+    <StyledActionButton onClick={() => addWeekToTrello(listOfDays)}>
+      Generer handleliste
+    </StyledActionButton>
+  );
+};
--- src/menu/Week.tsx
+++ src/menu/Week.tsx
@@ -1,21 +1,12 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { Day } from "./Day";
 import { startOfWeek, getISOWeek, addDays, subWeeks, addWeeks } from "date-fns";
 import styled from "styled-components";
-import {
-  StyledActionButtonWithMargins,
-  StyledActionButton
-} from "../components/StyledActionButton";
+import { StyledActionButtonWithMargins } from "../components/StyledActionButton";
 import { StyledWideWrapper } from "../components/StyledWrapper";
-import { start } from "repl";
 import { StyledHeaderH1 } from "../components/StyledHeaderH1";
-import { StyledNotification } from "../components/StyledNotification";
 import { StyledNext, StyledPrevious } from "../components/StyledSvgIcons";
-import { UserDataContext, UserData } from "../context/UserDataContext";
-import { Ingredient, RecipeType } from "../types";
-import { firebase } from "../firebase/firebase";
-import { RecipeContext } from "../context/RecipeContext";
-import { IngredientsContext } from "../context/IngredientsContext";
+import { AddToTrello } from "./AddToTrello";
 
 const StyledDayList = styled.div`
   display: flex;
@@ -28,111 +19,7 @@ const StyledButtonGroup = styled.div`
   justify-content: space-between;
 `;
 
-interface CreateCardResponse {
-  id: string;
-}
-interface CreateChecklistResponse {
-  id: string;
-}
-
-const addIngredients = (
-  ingredients: string[],
-  userdata: UserData,
-  checklistId: string
-) => {
-  ingredients.forEach(ingredient => {
-    fetch(
-      `https://api.trello.com/1/checklists/${checklistId}/checkItems?key=${
-        userdata.trelloApiKey
-      }&token=${userdata.trelloApiToken}&name=${ingredient}`,
-      {
-        method: "POST"
-      }
-    );
-  });
-};
-
-const addChecklist = (
-  ingredients: string[],
-  userdata: UserData,
-  id: string
-) => {
-  fetch(
-    `https://api.trello.com/1/checklists/?key=${userdata.trelloApiKey}&token=${
-      userdata.trelloApiToken
-    }&idCard=${id}`,
-    {
-      method: "POST"
-    }
-  )
-    .then(data => data.json())
-    .then((data: CreateChecklistResponse) => {
-      addIngredients(ingredients, userdata, data.id);
-    });
-};
-
-const addCard = (ingredients: string[], userdata: UserData) => {
-  fetch(
-    `https://api.trello.com/1/cards?name=Handleliste&pos=top&idList=${
-      userdata.trelloList
-    }&key=${userdata.trelloApiKey}&token=${userdata.trelloApiToken}`,
-    {
-      method: "POST"
-    }
-  )
-    .then((data: any) => data.json())
-    .then((data: CreateCardResponse) => {
-      addChecklist(ingredients, userdata, data.id);
-    });
-};
-
-const addWeekToTrello = (
-  ingredients: Ingredient[],
-  recipes: RecipeType[],
-  listOfDays: Date[],
-  userdata: UserData
-) => {
-  const db = firebase.firestore();
-  const promiseList: any = [];
-  listOfDays.forEach((day: any) => {
-    promiseList.push(
-      db
-        .collection("days")
-        .where("date", "==", day)
-        .get()
-    );
-  });
-
-  Promise.all(promiseList).then(data => {
-    const res = data.reduce((init: any, next: any) => {
-      init.push(next.docs[0].data());
-      return init;
-    }, []);
-    const recipeIds = res
-      .filter((el: any) => el.recipe)
-      .map((el: any) => el.recipe);
-    const recipesThisWeek: RecipeType[] = recipeIds.map((recipeId: string) =>
-      recipes.find(el => el.id === recipeId)
-    );
-
-    const ingredientsThisWeek = recipesThisWeek
-      .reduce((init: any, recipe: RecipeType) => {
-        const ingredientsOnThisRecipe = recipe.ingredients.map(ingredientId =>
-          ingredients.find(el => el.id === ingredientId)
-        );
-
-        return init.concat(ingredientsOnThisRecipe);
-      }, [])
-      .map((ingredient: Ingredient) => ingredient.name);
-
-    addCard(ingredientsThisWeek, userdata);
-  });
-};
-
 export const Week = () => {
-  const userdata = useContext(UserDataContext).userdata;
-  const recipes = useContext(RecipeContext).recipes;
-  const ingredients = useContext(IngredientsContext).ingredients;
   const [selectedDay, setSelectedDay] = useState(
     startOfWeek(new Date(), { weekStartsOn: 1 })
   );
@@ -143,14 +30,7 @@ export const Week = () => {
 
   return (
     <StyledWideWrapper>
-      <StyledActionButton
-        onClick={() =>
-          addWeekToTrello(ingredients, recipes, listOfDays, userdata)
-        }
-      >
-        Generer
-      </StyledActionButton>
-
+      <AddToTrello listOfDays={listOfDays} />
       <StyledHeaderH1>Ukesmeny uke {getISOWeek(selectedDay)} </StyledHeaderH1>
       <StyledButtonGroup>
         <StyledActionButtonWithMargins
