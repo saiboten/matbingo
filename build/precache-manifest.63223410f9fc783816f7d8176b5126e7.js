c965f9d9fdee43481cd8da251aecafa4cfd6a809
saiboten <saiboten@gmail.com>
2019-03-15 22:55:05 +0100

    For now I actually need to .. add the build

---
+++ build/precache-manifest.63223410f9fc783816f7d8176b5126e7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e565580d089603b515a56ffdaf06769f",
+    "url": "/static/media/arrow-left.e565580d.svg"
+  },
+  {
+    "revision": "459d374387fc45d5b9c6",
+    "url": "/static/js/main.459d3743.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9ce825785a08eb598f7bdf8ad6d1631a",
+    "url": "/static/media/plus-circle.9ce82578.svg"
+  },
+  {
+    "revision": "02706417516b1cc58ce0616870ee94ed",
+    "url": "/static/media/dice.02706417.svg"
+  },
+  {
+    "revision": "0054b33c5fd6e00a8a9f722a56855a96",
+    "url": "/static/media/search.0054b33c.svg"
+  },
+  {
+    "revision": "0627dfc4fa49df29a2f638d24b950bd1",
+    "url": "/static/media/edit.0627dfc4.svg"
+  },
+  {
+    "revision": "1edf7dcd39dfcb405ba5b8b0513fd8bf",
+    "url": "/static/media/arrow-right.1edf7dcd.svg"
+  },
+  {
+    "revision": "f8d69415b609245ff97e9025040cf363",
+    "url": "/static/media/google.f8d69415.svg"
+  },
+  {
+    "revision": "07817711fb7fcdcbeca9612e3e809d1d",
+    "url": "/static/media/back.07817711.svg"
+  },
+  {
+    "revision": "f7cd296b2a49b35aa5386cae34c94f14",
+    "url": "/static/media/check.f7cd296b.svg"
+  },
+  {
+    "revision": "7b0fe074b587ca398c1c072890d03dd7",
+    "url": "/static/media/rotate-ccw.7b0fe074.svg"
+  },
+  {
+    "revision": "24d46f1c594de3aa58e9a1f51c6f95af",
+    "url": "/static/media/trash-2.24d46f1c.svg"
+  },
+  {
+    "revision": "100738e6f15853e06ebc62487422b981",
+    "url": "/static/media/Loading.100738e6.svg"
+  },
+  {
+    "revision": "cb438eac83322313fe88",
+    "url": "/static/js/1.cb438eac.chunk.js"
+  },
+  {
+    "revision": "c5b3779b5c9f6901656237328e7e4ee2",
+    "url": "/index.html"
+  }
+];
