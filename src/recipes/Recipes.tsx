8bccc0682793eb07c9fb8e0282c4c0068996ece9
saiboten <saiboten@gmail.com>
2019-04-28 22:15:04 +0200

    Preparing to move list recipes and add recipes to new pages

--- src/recipes/Recipes.tsx
+++ src/recipes/Recipes.tsx
@@ -5,6 +5,8 @@ import { StyledWrapper } from "../components/StyledWrapper";
 import { RouteComponentProps } from "react-router";
 import { StyledNotification } from "../components/StyledNotification";
 import { ListRecipesAndRedirect } from "./ListRecipesAndRedirect";
+import { StyledButtonWithMargins } from "../components/StyledButton";
+import { StyledHeaderH1 } from "../components/StyledHeaderH1";
 
 interface MatchParams {
   feedback: string;
@@ -12,12 +14,10 @@ interface MatchParams {
 
 interface Props extends RouteComponentProps<MatchParams> {}
 
-export const Recipes = ({
-  match: {
-    params: { feedback }
-  }
-}: Props) => {
+export const RecipesContent = ({ feedback }: { feedback: string }) => {
   const [feedbackActive, setFeedbackActive] = useState(false);
+  const [showAddRecipe, setShowAddRecipe] = useState(false);
+  const [showFindRecipe, setShowFindRecipe] = useState(false);
 
   useEffect(() => {
     if (feedback) {
@@ -31,10 +31,36 @@ export const Recipes = ({
   }, []);
 
   return (
-    <StyledWrapper backgroundColor="white">
+    <>
       <StyledNotification text="Oppskrift slettet" active={feedbackActive} />
-      <ListRecipesAndRedirect />
-      <AddRecipe />
+      <StyledHeaderH1>Oppskrifter</StyledHeaderH1>
+      {showFindRecipe ? (
+        <ListRecipesAndRedirect />
+      ) : (
+        <StyledButtonWithMargins onClick={() => setShowFindRecipe(true)}>
+          Finn oppskrift
+        </StyledButtonWithMargins>
+      )}
+
+      {showAddRecipe ? (
+        <AddRecipe />
+      ) : (
+        <StyledButtonWithMargins onClick={() => setShowAddRecipe(true)}>
+          Legg til oppskrift
+        </StyledButtonWithMargins>
+      )}
+    </>
+  );
+};
+
+export const Recipes = ({
+  match: {
+    params: { feedback }
+  }
+}: Props) => {
+  return (
+    <StyledWrapper backgroundColor="white">
+      <RecipesContent feedback={feedback} />
     </StyledWrapper>
   );
 };
